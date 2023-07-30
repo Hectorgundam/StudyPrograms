4a8a339608f8ca45e9beeaecf81b7f706d6869fa
Hectorgundam <140485276+Hectorgundam@users.noreply.github.com>
2023-07-30 00:20:58 -0400

    Edited structure, added study programs

---
+++ Python Madlib/Python Madlib.py
@@ -0,0 +1,15 @@
+# Python Madlib
+
+adj = input("Adjective: ")
+
+verb1 = input("Verb: ")
+
+verb2 = input("Verb: ")
+
+famousPerson = input("Famous person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because\
+ I love to {verb1}. Stay hydrated and {verb2} like you are {famousPerson}!"
+
+print(madlib)
+
---
+++ daysInBetweenCalculator/daysInBetweenCalculator.py
@@ -0,0 +1,12 @@
+# Python program
+# Description
+# Python program that calculates and prints the number of days between two given dates
+
+# The user should be able t oenter the two dates (one per line) with this format Year Month Day 
+# The year must be formatted with for digits - Example 2021
+# The month must be an integer between 1 and 12 (with no leading zeros) 
+# The day must be an integer between 1 and 31 (with no leading zeros) 
+# The first date must be previous or equal to the second date
+# If the dates are equal (there are 0 days between them) display the message "The dates are equal"
+# If there is only one day between the two dates, display the message "There is only 1 day between these dates."
+# If the first date is later than the second date display the message "Please enter valid dates"
