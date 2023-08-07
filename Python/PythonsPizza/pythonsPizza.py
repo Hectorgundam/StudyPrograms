447138526c6fc9ff2a1372910489fafa69fae4bc
Hectorgundam <140485276+Hectorgundam@users.noreply.github.com>
2023-08-06 22:15:10 -0400

    Create pythonsPizza.py

---
+++ Python/PythonsPizza/pythonsPizza.py
@@ -0,0 +1,21 @@
+# Pythons Pizza
+
+# Description 
+# A program where the user can order pizza and customize their toppings 
+
+# Small pizza $15
+# Medium pizza $20 
+# Large pizza $25
+
+# Add pepperoni on small is $2 and on medium or large is $3
+# Add extra cheese any size is $1
+
+# Intro 
+print("Welcome to Python's Pizza! Lets get your order started.")
+
+# Asking the user what item they would like to order from the menu
+addItem = input("What would you like to order?")
+
+
+
+
